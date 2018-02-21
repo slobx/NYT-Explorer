a09f1c2e52e05179df9f751397c017c3d0342b43
Slobodan Vrhovac <slobxns@gmail.com>
2018-02-20 19:21:22 -0800

    Initial commit

---
+++ js/script.jsx
@@ -0,0 +1,10 @@
+class App extends React.Component {
+    
+    render(){
+        return(
+            <h1>NYT Explorer</h1>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
